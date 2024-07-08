8cd586c6f8d368db9b68485cbf1c5ca80d4fe33b
hruntov <hruntov@outlook.com>
2024-07-08 14:55:06 +0300

    Create product_list func that display a list of products filtered by a given category

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Category, Product
 
-# Create your views here.
+
+def product_list(request, category_slug=None):
+    """
+    Display a list of products, optionally filtered by a given category.
+
+    Args:
+        request: HttpRequest object.
+        category_slug (str, optional): Slug of the category to filter products by. Defaults to None.
+
+    Returns:
+        HttpResponse: Rendered HTML page with the list of products, along with the current category
+            and all categories.
+
+    """
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+    return render(request, 'shop/product/list.html', {'category': category,
+                                                      'categories': categories,
+                                                      'products': products})
