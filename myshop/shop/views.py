5e45e27aa18fde53b9fadc67ada53dd831ea4762
hruntov <hruntov@outlook.com>
2024-07-08 19:08:07 +0300

    Add detail page to the website

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -40,4 +40,4 @@ def product_detail(request, id, slug):
 
     """
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
-    return render(request, 'shop,product,detail.html', {'product': product})
+    return render(request, 'shop/product/detail.html', {'product': product})
