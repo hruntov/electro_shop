7eda0b0ec9a9b884cf36b4f64ddf90b70976d9fc
hruntov <hruntov@outlook.com>
2024-07-21 23:02:41 +0300

    Create celery task after order creation

---
+++ myshop/orders/tasks.py
@@ -0,0 +1,16 @@
+from django.core.mail import send_mail
+
+from celery import shared_task
+
+from .models import Order
+
+
+@shared_task
+def order_created(order_id):
+    order = Order.objects.get(id=order_id)
+    subject = f'Замовлення номер {order.id}'
+    message = f'Дорогий {order.first_name}, \n\n' \
+              f'Ми прийняли ваше замовлення.' \
+              f'Номер вашого замовлення - {order.id}'
+    mail_sent = send_mail(subject, message, 'admin@myshop.com', [order.email])
+    return mail_sent
--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from cart.cart import Cart
 from .forms import OrderCreateForm
 from .models import OrderItem
+from .tasks import order_created
 
 
 def order_create(request):
@@ -19,6 +20,7 @@ def order_create(request):
                                              price=item['price'],
                                              quantity=item['quantity'])
                 cart.clear()
+                order_created.delay(order.id)
             return render(request, 'orders/order/created.html', {'order': order})
     else:
         form = OrderCreateForm()
