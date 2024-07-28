90e88c4433f6075b3a30483633551311696118e4
hruntov <hruntov@outlook.com>
2024-07-28 14:50:16 +0300

    Create fixture for db with default data and superuser

--- start_web.sh
+++ start_web.sh
@@ -2,6 +2,8 @@
 
 cd myshop && \
 python manage.py migrate
+python manage.py loaddata shop/fixtures/shop.json
+python manage.py loaddata myshop/fixtures/users.json
 python manage.py clearcache | python manage.py shell
 celery -A myshop worker -l info &
 python manage.py runserver 0.0.0.0:8000
