d57b0685ce5aa6072362b9cf4c0592659dc57ac2
hruntov <hruntov@outlook.com>
2024-07-28 16:27:10 +0300

    Add flower framework for monitoring celery queue

--- start_web.sh
+++ start_web.sh
@@ -6,4 +6,5 @@ python manage.py loaddata shop/fixtures/shop.json
 python manage.py loaddata myshop/fixtures/users.json
 python manage.py clearcache | python manage.py shell
 celery -A myshop worker -l info &
+celery -A myshop flower --port=5555 &
 python manage.py runserver 0.0.0.0:8000
