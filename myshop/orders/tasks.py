e4c22a6d7b6a72b53054f407d554b666a2a8e4f1
hruntov <hruntov@outlook.com>
2024-08-03 19:43:32 +0300

    Add anotation and docstrinc for order_created func

--- myshop/orders/tasks.py
+++ myshop/orders/tasks.py
@@ -6,7 +6,17 @@ from .models import Order
 
 
 @shared_task
-def order_created(order_id):
+def order_created(order_id: int) -> int:
+    """
+    Send an email notification when an order is created.
+
+    Args:
+        order_id (int): The ID of the order that was created.
+
+    Returns:
+        int: The number of successfully delivered messages (1 if successful, 0 otherwise).
+
+    """
     order = Order.objects.get(id=order_id)
     subject = f'Замовлення номер {order.id}'
     message = f'Дорогий {order.first_name}, \n\n' \
