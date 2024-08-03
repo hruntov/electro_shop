f651abab58fa8be7b4ef1d74f242dd49bdd7cec2
hruntov <hruntov@outlook.com>
2024-08-03 19:48:31 +0300

    Add the docstring and anotation to the cart context processor

--- myshop/cart/context_processors.py
+++ myshop/cart/context_processors.py
@@ -1,5 +1,20 @@
+from typing import Dict
+
+from django.http import HttpRequest
+
 from .cart import Cart
 
 
-def cart(request):
+def cart(request: HttpRequest) -> Dict[str, Cart]:
+    """
+    This context processor adds the cart instance to the context, making it available in all
+        templates.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        Dict[str, Cart]: A dictionary containing the cart instance.
+
+    """
     return {'cart': Cart(request)}
