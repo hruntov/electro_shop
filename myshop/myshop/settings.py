620a8ccfa63757751dd68f4a637adcbc507ee8a2
hruntov <hruntov@outlook.com>
2024-08-12 14:56:44 +0300

    Add static_root path to the settings

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -128,7 +128,8 @@ INTERNAL_IPS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
