b28319f9ee925c82699c462c79d68c7fc70843e3
hruntov <hruntov@outlook.com>
2024-08-12 14:57:27 +0300

    Add collectstatic command to the starting script

--- start_web.sh
+++ start_web.sh
@@ -5,6 +5,7 @@ python manage.py migrate
 python manage.py loaddata shop/fixtures/shop.json
 python manage.py loaddata myshop/fixtures/users.json
 python manage.py clearcache | python manage.py shell
+python manage.py collectstatic --noinput
 celery -A myshop worker -l info &
 celery -A myshop flower --port=5555 &
 python manage.py runserver 0.0.0.0:8000
