0f5d26af7cf37a2b7d0a34d07bd63c4bd2827a9d
hruntov <hruntov@outlook.com>
2024-08-15 14:22:15 +0300

    Add the calculating price of the order with discount

--- myshop/orders/models.py
+++ myshop/orders/models.py
@@ -37,8 +37,32 @@ class Order(models.Model):
         return f'Order {self.id}'
 
     def get_total_cost(self) -> Union[float, int]:
-        """Calculates the total cost of the order by summing the cost of each item in the order."""
+        """Calculates the total cost of the order."""
+        total_cost = self.get_total_cost_before_discount()
+        return total_cost - self.get_discount()
+
+    def get_total_cost_before_discount(self) -> Decimal:
+        """
+        Calculate the total cost of items in the cart before applying any discounts.
+
+        Returns:
+            Decimal: The total cost of all items in the cart before any discounts.
+
+        """
         return sum(item.get_cost() for item in self.items.all())
+
+    def get_discount(self) -> Decimal:
+        """
+        Calculate the discount amount based on the total cost before discount.
+
+        Returns:
+            Decimal: The discount amount. Returns 0 if no discount is available.
+
+        """
+        total_cost = self.get_total_cost_before_discount()
+        if self.discount:
+            return total_cost * (self.discount / Decimal(100))
+        return Decimal(0)
 
 
 class OrderItem(models.Model):
