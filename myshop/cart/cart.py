39c15bbd2247b87237fc025e7c9c9d833d459410
hruntov <hruntov@outlook.com>
2024-08-15 14:00:57 +0300

    Add the cart's functions for calculate coupon's discount

--- myshop/cart/cart.py
+++ myshop/cart/cart.py
@@ -1,9 +1,10 @@
 from decimal import Decimal
-from typing import Any, Dict, Iterator
+from typing import Any, Dict, Iterator, Optional
 
 from django.conf import settings
 from django.http import HttpRequest
 
+from coupons.models import Coupon
 from shop.models import Product
 
 
@@ -23,6 +24,7 @@ class Cart:
         if not cart:
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
+        self.coupon_id = self.session.get('coupon_id')
 
     def __iter__(self) -> Iterator[Dict[str, Any]]:
         """
@@ -108,3 +110,23 @@ class Cart:
         """Clears all items from the cart session."""
         del self.session[settings.CART_SESSION_ID]
         self.save()
+
+    @property
+    def coupon(self) -> Optional[Coupon]:
+        """Returns the Coupon object if the coupon_id is valid, otherwise returns None."""
+        if self.coupon_id:
+            try:
+                return Coupon.objects.get(id=self.coupon_id)
+            except Coupon.DoesNotExist:
+                pass
+        return None
+
+    def get_discount(self) -> Decimal:
+        """Calculates the discount amount based on the coupon's discount percentage."""
+        if self.coupon:
+            return (self.coupon.discount / Decimal(100)) * self.get_total_price()
+        return Decimal(0)
+
+    def get_total_price_after_discount(self) -> Decimal:
+        """Calculates the total price after applying the discount."""
+        return self.get_total_price() - self.get_discount()
