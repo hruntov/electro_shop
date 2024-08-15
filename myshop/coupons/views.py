c06b395cd10f7dc390adc7e0878eb70c3e4e13f3
hruntov <hruntov@outlook.com>
2024-08-15 13:59:18 +0300

    Add the coupon_apply func for apply coupon for the current cart session

--- myshop/coupons/views.py
+++ myshop/coupons/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import redirect, render
+from django.utils import timezone
+from django.views.decorators.http import require_POST
 
-# Create your views here.
+from .forms import CouponApplyForm
+from .models import Coupon
+
+
+@require_POST
+def coupon_apply(request: HttpRequest) -> HttpResponse:
+    """
+    Apply a coupon to the current cart session.
+
+    Args:
+        request (HttpRequest): The HTTP request object containing POST data with the coupon code.
+
+    Returns:
+        HttpResponse: A redirect response to the cart detail page.
+
+    """
+    now = timezone.now()
+    form = CouponApplyForm(request.POST)
+    if form.is_valid():
+        code = form.cleaned_data['code']
+        try:
+            coupon = Coupon.objects.get(code__iexact=code,
+                                        valid_from__lte=now,
+                                        valid_to__gte=now,
+                                        active=True)
+            request.session['coupon_id'] = coupon.id
+        except Coupon.DoesNotExist:
+            request.session['coupon_id'] = None
+    return redirect('cart:cart_detail')
--- myshop/myshop/urls.py
+++ myshop/myshop/urls.py
@@ -22,6 +22,7 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/', include('cart.urls', namespace='cart')),
+    path('coupons/', include('coupons.urls', namespace='coupons')),
     path('orders/', include('orders.urls', namespace='orders')),
     path('payment/', include('payment.urls', namespace='payment')),
     path('', include('shop.urls', namespace='shop')),
