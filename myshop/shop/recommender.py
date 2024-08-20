d752162b2b0a55f5ddaa56099b126dde85d90a61
hruntov <hruntov@outlook.com>
2024-08-20 14:46:16 +0300

    Add the suggest_products_for func for return suggested products

--- myshop/shop/recommender.py
+++ myshop/shop/recommender.py
@@ -39,3 +39,37 @@ class Recommender:
             for with_id in product_ids:
                 if product_id != with_id:
                     r.zincrby(self.get_product_key(product_id), 1, with_id)
+
+    def suggest_products_for(self, products: List[Product], max_results: int = 6) -> List[Product]:
+        """
+        Suggest products for a given list of products.
+
+        This method takes a list of products and returns a list of suggested products with scores
+        based on the input products.
+
+        Args:
+            products (list): A list of Product objects for which suggestions are to be made.
+            max_results (int, optional): The maximum number of suggested products to return.
+
+        Returns:
+            list: A list of suggested Product objects.
+
+        """
+        product_ids = [p.id for p in products]
+
+        if len(products) == 1:
+            suggestions = r.zrange(
+                self.get_product_key(product_ids[0]), 0, -1, desc=True)[:max_results]
+        else:
+            flat_ids = ''.join([str(id) for id in product_ids])
+            tmp_key = f'tmp_{flat_ids}'
+            keys = [self.get_product_key(id) for id in product_ids]
+            r.zunionstore(tmp_key, keys)
+            r.zrem(tmp_key, *product_ids)
+            suggestions = r.zrange(tmp_key, 0, -1, desc=True)[:max_results]
+            r.delete(tmp_key)
+
+        suggested_products_ids = [int(id) for id in suggestions]
+        suggested_products = list(Product.objects.filter(id__in=suggested_products_ids))
+        suggested_products.sort(key=lambda x: suggested_products_ids.index(x.id))
+        return suggested_products
